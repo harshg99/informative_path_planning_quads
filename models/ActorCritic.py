30842addbf18d4115c92d1f4743057c94e1956d2
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-06-02 03:22:22 -0400

    polciy loss corrections

--- models/ActorCritic.py
+++ models/ActorCritic.py
@@ -99,7 +99,7 @@ class ActorCritic(Vanilla):
         target_v = torch.tensor(target_v, dtype=torch.float32)
         a_batch = torch.tensor(a_batch, dtype=torch.int64)
         advantages = torch.tensor(advantages, dtype=torch.float32)
-        #advantages = (advantages - advantages.mean(axis=-1))/(advantages.std(axis=-1)+1e-8)
+        advantages = (advantages - advantages.mean(axis=-1))/(advantages.std(axis=-1)+1e-8)
         old_policy = torch.tensor(old_policy,dtype=torch.float32)
 
         responsible_outputs = policy.gather(-1, a_batch)
@@ -226,7 +226,7 @@ class ActorCritic3(ActorCritic2):
         target_v = torch.tensor(target_v, dtype=torch.float32)
         a_batch = torch.tensor(a_batch, dtype=torch.int64)
         advantages = torch.tensor(advantages, dtype=torch.float32)
-        #advantages = (advantages - advantages.mean(axis=-1)) / (advantages.std(axis=-1) + 1e-8)
+        advantages = (advantages - advantages.mean(axis=-1)) / (advantages.std(axis=-1) + 1e-8)
 
         old_policy = torch.tensor(old_policy.squeeze(),dtype=torch.float32)
 
