0a65c520fd578e760e5e1227b1ef95378bab4126
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-08-17 18:02:30 -0400

    coverage planner written

--- baseline_params/CoverageGPParams.py
+++ baseline_params/CoverageGPParams.py
@@ -1,4 +1,4 @@
 depth = 2
 GIFS = True
-GIFS_PATH = "data/baseline_results/coverage/GIFS/depth1"
-RESULTS_PATH="data/baseline_results/coverage/RESULTS/depth1"
+GIFS_PATH = "data/baseline_results/coverage/GIFS/depth2"
+RESULTS_PATH="data/baseline_results/coverage/RESULTS/depth2"
