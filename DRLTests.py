855863ac5344fd8655d9fb7977df1a1614385538
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-08-31 16:22:22 -0400

    completed accurate baselines

--- DRLTests.py
+++ DRLTests.py
@@ -99,12 +99,14 @@ class Tests:
             rewardmap = np.load(file_name)
             file_name = dir_name + "tests{}target.npy".format(testID)
             targetmap = np.load(file_name)
+            file_name =dir_name+"tests{}target_orig_dist.npy".format(testID)
+            origtargetmap=np.load(file_name)
         else:
             file_name = dir_name + "tests{}.npy".format(testID)
             rewardmap = np.load(file_name)
             targetmap  = None
 
-        return drl_planner.run_test(rewardmap,testID,targetmap)
+        return drl_planner.run_test(rewardmap,testID,targetmap,orig_target_map_dist=origtargetmap)
 
     def run_tests(self,type:str,num_tests:int,num_threads:int,mapSize:int):
         with mp.Pool(num_threads) as pool:
