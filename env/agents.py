1524a94271087adc3f1617b55645d11b482d95cc
hydrargyrum5799 <hgoel1000p@gmail.com>
2022-12-29 16:40:32 -0500

    Agents footprint added for semantic mapping

--- env/agents.py
+++ env/agents.py
@@ -11,6 +11,7 @@ from motion_primitives_py import MotionPrimitiveLattice
 from copy import deepcopy
 import os
 from env.sensors import *
+from env.GPSemantic import GPSemanticMap
 
 class Agent():
     def __init__(self,ID,row,col,map_size,pad,world_size):
@@ -188,11 +189,33 @@ class AgentGP(AgentMP):
 
 
 
-class AgentSemantic(AgentMP):
-    def __init__(self,ID,row,col,map_size,pad,world_size,mp_graph,lookup,spatial_dim,tiles,sensor_params,budget=None):
-        super(AgentGP, self).__init__(ID,row,col,map_size,pad,world_size,mp_graph,lookup,spatial_dim,tiles,budget)
-        self.beliefMap = np.zeros([self.world_size,self.world_size])
-        self.targetMap = np.zeros([self.world_size,self.world_size])
+class AgentSemantic :
+    def __init__(self,ID = 0,
+                 pos = None,
+                 groundTruth = None,
+                 mp_object = None,
+                 sensor_params = None,
+                 budget=None):
+
+        self.ID = ID
+        self.pos = np.array(pos)
+
+        self.pad = groundTruth.padding
+
+        self.tiles = mp_object.num_tiles
+
+        self.worldMap = None
+        self.visited_states = None
+        self.mp_graph = mp_object.mp_graph
+        self.lookup = mp_object.lookup
+        self.index = 0
+        self.spatial_dim = mp_object.spatial_dim
+
+        self.prev_action = 0
+        self.pos_actual = self.pos.copy()
+
+        self.localSemanticMap = np.zeros([self.world_size,self.world_size])
+        self.worldSemanticMap = np.zeros([self.world_size,self.world_size])
         self.sensor = sensor_setter.set_env(sensor_params)
 
 
@@ -203,6 +226,81 @@ class AgentSemantic(AgentMP):
     def updatePos(self,action,beliefThresh = 0.99):
         valid,states,cost = super(AgentGP, self).updatePos(action)
         return valid,states,cost
+
+
+    def updateMap(self,worldMap):
+        self.worldMap= worldMap
+
+    def get_mp(self,action):
+        mp = deepcopy(self.mp_graph[self.index, action])
+        return mp
+
+    def updatePos(self,action):
+        mp = deepcopy(self.mp_graph[self.index, action])
+        mpcost = 5000
+        if mp is not None:
+            #mp.translate_start_position(self.pos)
+            #_, sp = mp.get_sampled_position()
+            # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
+            #visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
+            is_valid,visited_states = self.isValidMP(mp)
+            mask = [np.all(visited_states[:,j]==self.pos) for j in range(visited_states.shape[-1])]
+            visited_states = np.delete(visited_states,np.where(mask),axis= -1)
+            self.prev_action = action
+            self.current_primitive = deepcopy(mp)
+            if is_valid:
+                next_index = self.lookup[self.index, action]
+                next_index = int(np.floor(next_index / self.tiles))
+                self.index = next_index
+                self.pos = np.round(mp.end_state[:self.spatial_dim]).astype(int)
+                self.pos_actual = mp.end_state[:self.spatial_dim]
+                #print("{:d} {:d} {:d} {:d}".format(self.pos[0], self.pos[1], visited_states[0,0], visited_states[1,0]))
+                self.visited_states = visited_states
+                for s in visited_states.T:
+                    self.trajectory[s[0],s[1]] = 1
+
+                mpcost = mp.cost / mp.subclass_specific_data.get('rho', 1) / 10
+                if self.agentBudget is not None:
+                    self.agentBudget -= mpcost
+                return is_valid, visited_states, mpcost
+            if self.agentBudget is not None:
+                self.agentBudget -= mpcost
+            return False,visited_states,None
+        if self.agentBudget is not None:
+            self.agentBudget -= mpcost
+        return False,None, None
+
+    def updatePosOdom(self,pos):
+        self.pos_actual = np.array(pos)
+        self.pos = np.round(self.pos).astype(int)
+
+    def isValidMP(self,mp):
+        is_valid = mp.is_valid
+        mp.translate_start_position(self.pos_actual)
+        _, sp = mp.get_sampled_position()
+        # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
+        visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
+        #is_valid = is_valid and self.isValidPoses(visited_states)
+        final_pos = np.round(mp.end_state[:self.spatial_dim]).astype(int)
+        is_valid = is_valid and self.isValidFinalPose(final_pos)
+        return is_valid,visited_states
+
+    def isValidPoses(self, poses):
+        is_valid = True
+        for state in poses.T:
+            is_valid = is_valid and self.isValidPos(state)
+        return is_valid
+
+    def isValidFinalPose(self, final_pose):
+        is_valid = True
+        is_valid = is_valid and self.isValidPos(final_pose.T)
+        return is_valid
+
+    def isValidPos(self, pos):
+        is_valid = (np.array(pos - self.pad) > -1).all()
+        is_valid = is_valid and (np.array(pos + self.pad) < self.world_size).all()
+        return is_valid
+
 
     def updateInfoTarget(self,visited_states,worldTargetMap,beliefThresh):
         measurement_list = []
