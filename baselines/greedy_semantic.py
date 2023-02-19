fd1c00fe81f76c78ef6bdc87c92dd40627574c58
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-02-19 17:54:01 -0500

    greedy semantic test v1

---
+++ baselines/greedy_semantic.py
@@ -0,0 +1,176 @@
+import json
+
+from env.GPEnvMP import *
+from env.env_setter import *
+import numpy as np
+from copy import deepcopy
+from params import *
+import os
+from Utilities import set_dict
+from env.render import *
+from multiprocessing import Pool as pool
+from baselines.il_wrapper import il_wrapper_semantic
+from env.GPEnvSemantic import *
+from env.SemanticMap import *
+
+class GreedySemantic(il_wrapper_semantic):
+    def __init__(self,params_dict,home_dir="/"):
+        super().__init__(home_dir)
+        self.test_params = params_dict
+
+        self.gifs = params_dict['GIFS']
+        self.gifs_path = params_dict['GIFS_PATH']
+        self.results_path = params_dict['RESULTS_PATH']
+        self.depth = params_dict['depth']
+        self.exploration = params_dict['depth']
+
+        if not os.path.isdir(self.gifs_path):
+            os.makedirs(self.gifs_path)
+        if not os.path.isdir(self.results_path):
+            os.makedirs(self.results_path)
+
+
+    def getExpectedEntropy(self, visited_states,world_map):
+
+        world_map_init = deepcopy(world_map)
+
+        for state in visited_states.tolist():
+
+            semantic_obs,_ = world_map.get_observations(state, fov=self.env.sensor_params['sensor_range'],
+                                                       scale=None, type='semantic',
+                                                       return_distance=False, resolution=None)
+
+            projected_measurement = np.argmax(semantic_obs, axis=-1)
+
+            world_map.update_semantics(state, projected_measurement, self.env.sensor_params)
+
+
+        init_entropy = world_map_init.get_entropy()
+        final_entropy = world_map.get_entropy()
+        entropy_reduction = init_entropy - final_entropy
+
+        return entropy_reduction/np.square(self.env.sensor_params['sensor_range'])
+        #return entropy_reduction
+
+    def getMean(self,visited_states,worldMap):
+        entropy_reduction = 0
+        for state in visited_states.tolist():
+
+            semantic_obs,_ = worldMap.get_observations(state, fov=self.env.sensor_params['sensor_range'],
+                                                       scale=None, type='semantic',
+                                                       return_distance=False, resolution=None)
+
+
+            entropy_reduction += semantic_obs.sum()
+
+        return entropy_reduction / np.square(self.env.sensor_params['sensor_range'])
+        #return entropy_reduction
+
+    def getmpcost(self,pos,index,action,agentID,worldMap):
+        mp = deepcopy(self.mp_graph[index, action])
+        reward = 0
+        next_pos = pos
+        next_index = index
+        is_valid = False
+        if mp is not None:
+            # mp.translate_start_position(self.pos)
+            # _, sp = mp.get_sampled_position()
+            # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
+            # visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
+            is_valid, visited_states = self.isValidMP(pos,mp,agentID)
+            reward  = 0
+            if is_valid:
+                next_index = self.lookup[index, action]
+                next_index = int(np.floor(next_index / self.num_tiles))
+                next_pos =  np.round(mp.end_state[:self.spatial_dim]).astype(int)
+                # print("{:d} {:d} {:d} {:d}".format(self.pos[0], self.pos[1], visited_states[0,0], visited_states[1,0]))
+                self.visited_states = visited_states.T
+                reward = self.exploration * self.getMean(self.visited_states,worldMap)+\
+                self.getExpectedEntropy(self.visited_states,worldMap)
+
+                reward -= mp.cost/ mp.subclass_specific_data.get('rho', 1) / 10 / REWARD.MP.value
+            elif visited_states is not None:
+                # reward += REWARD.COLLISION.value*(visited_states.shape[0]+1)
+                reward += REWARD.COLLISION.value
+            else:
+                reward += REWARD.COLLISION.value * 1.5
+
+        return reward,next_index,next_pos,is_valid
+
+    def plan_action(self,pos,index,agentID,current_depth=0,worldMap=None):
+        if current_depth>=self.depth:
+            return self.env.getEntropy(worldMap.copy()).mean()
+        else:
+            costs = []
+            for j in range(self.env.action_size):
+                worldMap_ = deepcopy(worldMap)
+                cost,next_index,next_pos,is_valid = self.getmpcost(pos,index,j,agentID,worldMap_)
+                if is_valid:
+                    costs.append(cost + self.plan_action(next_pos,next_index,agentID,current_depth+1,worldMap_))
+                else:
+                    costs.append(cost + -100000)
+            if current_depth==0:
+                best_action = np.argmax(np.array(costs))
+                return best_action,np.max(np.array(costs))
+            else:
+                return np.max(np.array(costs))
+
+    '''
+    For imitation learning
+    '''
+    def return_action(self,agentID):
+        agent = self.env.agents[agentID]
+        action, cost = self.plan_action(deepcopy(agent.pos), deepcopy(agent.index), agentID)
+        return action,cost
+
+    def run_test(self,test_map_ID, test_ID):
+        '''
+        Run a test episode
+        @param test_map_ID: ID of the test map (0-24)
+        @param test_ID: testing ID for this set of map (0-ENV_PARAMS['TESTS_PER_MAP']-1)
+        '''
+
+        episode_step = 0.0
+        episode_rewards = 0.0
+        self.env.reset(test_ID,test_map_ID)
+        frames = []
+        done = False
+
+
+        # kl_divergence = np.mean(self.env.worldBeliefMap*np.log(
+        #     np.clip(self.env.worldBeliefMap,1e-10,1)/np.clip(orig_target_map_dist,1e-10,1)
+        # ))
+        #kl_divergence = np.mean(np.square(self.env.worldBeliefMap - orig_target_map_dist))
+
+        while ((not self.args_dict['FIXED_BUDGET'] and episode_step < self.env.episode_length) \
+               or (self.args_dict['FIXED_BUDGET'])):
+            if self.gifs:
+                frames.append(self.env.render(mode='rgb_array'))
+            action_dict = {}
+            worldMap = deepcopy(self.env.belief_semantic_map)
+            for j,agent in enumerate(self.env.agents):
+                action_dict[j],cost = self.plan_action(deepcopy(agent.pos),deepcopy(agent.index),j,worldMap=worldMap)
+            rewards,done = self.env.step_all(action_dict)
+            episode_rewards += np.array(rewards).sum()
+            episode_step+=1
+            if done:
+                break
+
+        metrics = self.env.get_final_metrics()
+        metrics['episode_reward'] = episode_rewards
+        metrics['episode_length'] = episode_step
+        metrics['proximity'] = self.env.proximity
+
+        if self.gifs:
+            make_gif(np.array(frames),
+                 '{}/episode_{:d}_{:d}_{:.1f}.gif'.format(self.gifs_path,
+                                                          test_map_ID*self.env_params['TESTS_PER_MAP']+test_ID ,
+                                                          0,
+                                                          episode_rewards))
+        return metrics
+
+if __name__=="__main__":
+    import baseline_params.GreedyGPparams as parameters
+    map_index = 20
+    planner = GreedySemantic(set_dict(parameters),home_dir='/../')
+    print(planner.run_test(test_map_ID=0,test_ID=0))
--- baselines/il_wrapper.py
+++ baselines/il_wrapper.py
@@ -48,3 +48,53 @@ class il_wrapper:
 
     def return_action(self, agentID):
         pass
+
+
+class il_wrapper_semantic:
+    def __init__(self,home_dir):
+
+        import env_params.Semantic as parameters
+        env_params_dict = set_dict(parameters)
+        env_params_dict['home_dir'] = os.getcwd() + home_dir
+        self.env_params_dict = env_params_dict
+        import params as args
+
+        args_dict = set_dict(args)
+        self.args_dict = args_dict
+        self.env = GPSemanticGym(env_params_dict,args_dict)
+
+        self.mp_graph = self.env.minimum_action_mp_graph
+        self.lookup = self.env.lookup_dictionary
+        self.num_tiles = self.env.mp_graph.num_tiles
+        self.spatial_dim = self.env.mp_graph.num_dims
+
+    def isValidMP(self,pos,mp,agentID):
+        is_valid = mp.is_valid
+        mp.translate_start_position(pos)
+        _, sp = mp.get_sampled_position()
+        # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
+        visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
+        final_pos = np.round(mp.end_state[:self.spatial_dim]).astype(int)
+        #is_valid = is_valid and self.isValidPoses(visited_states, agentID)
+        is_valid = is_valid and self.isValidFinalPose(final_pos.T,agentID)
+        return is_valid,visited_states
+
+    def isValidPoses(self, poses, agentID):
+        is_valid = True
+        for state in poses.T:
+            is_valid = is_valid and self.isValidPos(state, agentID)
+        return is_valid
+
+    def isValidPos(self, pos, agentID):
+        is_valid = (np.array(pos - self.env.agents[agentID].pad) > -1).all()
+        is_valid = is_valid and (np.array(pos + self.env.agents[agentID].pad) \
+                                 < self.env.agents[agentID].world_size).all()
+        return is_valid
+
+    def isValidFinalPose(self, final_pose,agentID):
+        is_valid = True
+        is_valid = is_valid and self.isValidPos(final_pose.T,agentID)
+        return is_valid
+
+    def return_action(self, agentID):
+        pass
