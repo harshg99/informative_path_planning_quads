corrected mp normalisation

baselines/il_wrapper.py
@@ -61,6 +61,7 @@ class il_wrapper_semantic:
         env_params_dict['home_dir'] =home_dir
         self.env_params_dict = env_params_dict
         self.measurement_step = env_params_dict['measurement_step']
+        self.sampled_step = env_params_dict['sampled_step_size']
 
         import params as args
 
@@ -80,29 +81,29 @@ class il_wrapper_semantic:
     def isValidMP(self, pos, mp, agentID):
         is_valid = mp.is_valid
         mp.translate_start_position(pos)
-        _, sp = mp.get_sampled_position()
+        _, sp = mp.get_sampled_position(step_size=self.sampled_step)
         # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
-        #visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
+        # visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
         visited_states = np.round(sp).astype(np.int32)
         visited_states_ = []
         dict_ = {}
-        for j in range(0,visited_states.shape[-1]):
-            if tuple(visited_states[:,j]) not in dict_.keys():
-                visited_states_.append(visited_states[:,j])
-                dict_[tuple(visited_states[:,j])] = 1.0
 
-        visited_states = np.array(visited_states_).T
-        visited_states_ = [visited_states[:,0]]
-        for j in range(1,visited_states.shape[1]-1,1):
-            if j%self.measurement_step==0:
-                visited_states_.append(visited_states[:,j])
-        visited_states_.append(visited_states[:,-1])
-        visited_states = np.transpose(np.array(visited_states_))
+        for j in range(0, visited_states.shape[-1]):
+            val = dict_.get(tuple(visited_states[:, j]), 0)
+            if val == 0:
+                visited_states_.append(visited_states[:, j])
+                dict_[tuple(visited_states[:, j])] = 1.0
 
-        #is_valid = is_valid and self.isValidPoses(visited_states)
-        final_pos = np.round(mp.end_state[:self.spatial_dim]).astype(int)
+        visited_states_ = np.array(visited_states_).T
+        indices = [0] + np.arange(self.measurement_step, visited_states_.shape[1] - 1, self.measurement_step).tolist() \
+                  + [visited_states_.shape[1] - 1]
+
+        visited_states = np.transpose(np.array(visited_states_)[:, indices])
+
+        # is_valid = is_valid and self.isValidPoses(visited_states)
         is_valid = is_valid and self.isValidFinalPose(visited_states, agentID)
         return is_valid,visited_states
+
 
     def isValidPoses(self, poses, agentID):
         is_valid = True

env/GPSemantic.py
@@ -158,7 +158,7 @@ class GPSemanticGym(gym.Env):
             self.seed = params_dict['seed']
         self.viewer = None
 
-        self.RANGE = int(self.args_dict['RANGE'] * self.params_dict['measurement_step']*\
+        self.RANGE = int(self.args_dict['RANGE'] * self.params_dict['obs_resolution']*\
                          self.params_dict['resolution']/self.args_dict['RESOLUTION'])
 
         if self.args_dict['OBSERVER'] == 'RANGE':
@@ -241,6 +241,7 @@ class GPSemanticGym(gym.Env):
 
         z = []
         costs = []
+        time = []
         for _ in self.mp_graph.edges:
             for edge in _:
                 if edge is not None:
@@ -249,6 +250,7 @@ class GPSemanticGym(gym.Env):
                     # print(np.linalg.norm(edge.end_state[0:2] - edge.start_state[0:2]))
                     z.append(np.linalg.norm(edge.end_state[0:2] - edge.start_state[0:2]))
                     costs.append(edge.cost)
+                    time.append(edge.traj_time)
         print(self.mp_graph.motion_primitive_type)
         print(np.mean(z))
         print([len([j for j in i if j != None]) for i in self.mp_graph.edges.T])
@@ -258,6 +260,9 @@ class GPSemanticGym(gym.Env):
         print(np.min(costs))
         print(np.max(costs))
         print(np.median(costs))
+
+        print(np.max(time))
+        print(np.min(time))
         self.mp_cost_norm = np.max(costs)
 
     def create_mp_graph_encodings(self,bits= True):
@@ -373,8 +378,8 @@ class GPSemanticGym(gym.Env):
 
 
     def get_obs_ranged(self,agentID):
-        fov_modified= [self.args_dict['RANGE']*self.params_dict['measurement_step'],
-                       self.args_dict['RANGE']*self.params_dict['measurement_step']]
+        fov_modified= [self.args_dict['RANGE']*self.params_dict['obs_resolution'],
+                       self.args_dict['RANGE']*self.params_dict['obs_resolution']]
         semantic_features,_ = self.belief_semantic_map.get_observations(pos =self.agents[agentID].pos,
                                                 fov = fov_modified,
                                                 scale = 1,
@@ -388,8 +393,8 @@ class GPSemanticGym(gym.Env):
 
 
     def get_obs_ranged_wobs(self,agentID):
-        fov_modified= [self.args_dict['RANGE']*self.params_dict['measurement_step'],
-                       self.args_dict['RANGE']*self.params_dict['measurement_step']]
+        fov_modified= [self.args_dict['RANGE']*self.params_dict['obs_resolution'],
+                       self.args_dict['RANGE']*self.params_dict['obs_resolution']]
 
         semantic_features,_ = self.belief_semantic_map.get_observations(pos =self.agents[agentID].pos,
                                                 fov=fov_modified,
@@ -407,8 +412,8 @@ class GPSemanticGym(gym.Env):
         return np.array(features)
 
     def get_obs_ranged_wobspenc(self,agentID):
-        fov_modified= [self.args_dict['RANGE']*self.params_dict['measurement_step'],
-                       self.args_dict['RANGE']*self.params_dict['measurement_step']]
+        fov_modified= [self.args_dict['RANGE']*self.params_dict['obs_resolution'],
+                       self.args_dict['RANGE']*self.params_dict['obs_resolution']]
 
         semantic_features, _ = self.belief_semantic_map.get_observations(pos=self.agents[agentID].pos,
                                                               fov=fov_modified,
@@ -441,8 +446,8 @@ class GPSemanticGym(gym.Env):
 
 
     def get_obs_range_wobs_multi(self,agentID):
-        fov_modified= [self.args_dict['RANGE']*self.params_dict['measurement_step'],
-                       self.args_dict['RANGE']*self.params_dict['measurement_step']]
+        fov_modified= [self.args_dict['RANGE']*self.params_dict['obs_resolution'],
+                       self.args_dict['RANGE']*self.params_dict['obs_resolution']]
 
         for s in self.scale:
             semantic_features, _ = self.belief_semantic_map.get_observations(pos=self.agents[agentID].pos,
@@ -475,8 +480,8 @@ class GPSemanticGym(gym.Env):
         return np.array(features)
 
     def get_obs_range_coverage_multifov(self,agentID):
-        fov_modified= [self.args_dict['RANGE']*self.params_dict['measurement_step'],
-                       self.args_dict['RANGE']*self.params_dict['measurement_step']]
+        fov_modified= [self.args_dict['RANGE']*self.params_dict['obs_resolution'],
+                       self.args_dict['RANGE']*self.params_dict['obs_resolution']]
 
         for s in self.scale:
             semantic_features, _ = self.belief_semantic_map.get_observations(pos=self.agents[agentID].pos,
@@ -621,15 +626,17 @@ class GPSemanticGym(gym.Env):
                         ground_truth= self.ground_truth_semantic_map,
                         mp_object = self.mp_object, \
                         sensor_params = self.sensor_params,
-                        budget = agent_budget) for j in range(self.numAgents)]
+                        budget = agent_budget,
+                        sampled_step = self.env_params['sampled_step_size']) for j in range(self.numAgents)]
         else:
             self.agents = [
-                AgentSemantic(ID = j, pos = [15*self.params_dict['measurement_step'],
-                                             15*self.params_dict['measurement_step']],
+                AgentSemantic(ID = j, pos = [15*self.params_dict['obs_resolution'],
+                                             15*self.params_dict['obs_resolution']],
                         ground_truth= self.ground_truth_semantic_map,
                         mp_object = self.mp_object, \
                         sensor_params = self.sensor_params,
-                        budget = agent_budget) for j in range(self.numAgents)]
+                        budget = agent_budget,
+                        sampled_step=self.env_params['sampled_step_size']) for j in range(self.numAgents)]
 
         # Initialising the semantic map for each agent
         for agent in self.agents:

env/agents.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 import os
 from env.sensors import *
 from env.SemanticMap import GPSemanticMap
-
+from collections import Counter
 class Agent():
     def __init__(self,ID,row,col,map_size,pad,world_size):
         self.ID = ID
@@ -195,7 +195,8 @@ class AgentSemantic :
                  ground_truth = None,
                  mp_object = None,
                  sensor_params = None,
-                 budget=None):
+                 budget=None,
+                 sampled_step = 0.1):
 
         self.ID = ID
         self.pos = np.array(pos)
@@ -211,6 +212,7 @@ class AgentSemantic :
         self.spatial_dim = mp_object.spatial_dim
         self.mp_cost_norm = mp_object.mp_cost_norm
         self.agent_budget = budget
+        self.sampled_step = sampled_step
 
         self.prev_action = 0
         self.pos_actual = self.pos.copy()
@@ -276,24 +278,24 @@ class AgentSemantic :
     def isValidMP(self,mp):
         is_valid = mp.is_valid
         mp.translate_start_position(self.pos_actual)
-        _, sp = mp.get_sampled_position()
+        _, sp = mp.get_sampled_position(step_size=self.sampled_step)
         # visited_states = np.round(mp.end_state[:mp.num_dims]).astype(np.int32).reshape(mp.num_dims,1)
         #visited_states = np.unique(np.round(sp).astype(np.int32), axis=1)
         visited_states = np.round(sp).astype(np.int32)
         visited_states_ = []
         dict_ = {}
+
         for j in range(0,visited_states.shape[-1]):
-            if tuple(visited_states[:,j]) not in dict_.keys():
+            val = dict_.get(tuple(visited_states[:,j]),0)
+            if val==0:
                 visited_states_.append(visited_states[:,j])
                 dict_[tuple(visited_states[:,j])] = 1.0
 
-        visited_states = np.array(visited_states_).T
-        visited_states_ = [visited_states[:,0]]
-        for j in range(1,visited_states.shape[1]-1,1):
-            if j%self.measurement_step==0:
-                visited_states_.append(visited_states[:,j])
-        visited_states_.append(visited_states[:,-1])
-        visited_states = np.transpose(np.array(visited_states_))
+        visited_states_ = np.array(visited_states_).T
+        indices = [0] + np.arange(self.measurement_step,visited_states_.shape[1]-1,self.measurement_step).tolist() \
+                  + [visited_states_.shape[1]-1]
+
+        visited_states = np.transpose(np.array(visited_states_)[:,indices])
 
         #is_valid = is_valid and self.isValidPoses(visited_states)
         final_pos = np.round(mp.end_state[:self.spatial_dim]).astype(int)