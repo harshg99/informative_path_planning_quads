fc0887f57326cfa4985e589ef8e86a540935bb4d
hydrargyrum5799 <hgoel1000p@gmail.com>
2023-07-20 16:58:27 -0400

    corrected the model parameters, removed a layer

--- model_params/MFModel4Seg.py
+++ model_params/MFModel4Seg.py
@@ -6,7 +6,7 @@ obs_model = 'MLP' #Conv
 if obs_model=='MLP':
     hidden_sizes = [256,128,64,64]
 else:
-    hidden_sizes = [16,32,64,64]
+    hidden_sizes = [16,32,64]
 
 pos_layer_size = [8,16]
 graph_node_layer_size = [32,64]
